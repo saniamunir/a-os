3ce07bd12b4cf31b2c97b4888881915339ede883
saniamunir <35691533+saniamunir@users.noreply.github.com>
2018-01-25 13:54:53 +0100

    Update Años bisiestos.py

--- Años bisiestos.py
+++ Años bisiestos.py
@@ -1,6 +1,12 @@
 # coding: utf-8
-anio = int(input("di el año:"))
-if (anio % 4 == 0 and anio % 100 =0) or a % 400 == 0):
- print "si es bisiestos"
-else:
- print "no es bisiestos"
+def esbisiesto(a):
+	if (a % 4 == 0 and a % 100 !=0 or a % 400 == 0):
+		print "Si es bisiesto"
+
+	else:
+		print "No es bisiesto"
+
+esbisiesto(2100)
+esbisiesto(2300)
+esbisiesto(1955)
+
